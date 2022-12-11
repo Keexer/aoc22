8a982f6ec5d5c8b2eb89e86420b82a013d1c5bca
Jacob Johansson <jacob.johansson@nexergroup.com>
2022-12-11 22:26:32 +0100

    Solved 11A

--- Days/include/Days/Day11.h
+++ Days/include/Days/Day11.h
@@ -1,7 +1,24 @@
 #pragma once
 
+#include <queue>
+#include <functional>
+
+struct Monkey
+{
+  std::queue<unsigned int> items{};
+  std::function<void(unsigned int&)> operation;
+  int dividable{};
+  int trueCondMonkey{};
+  int falseCondMonkey{};
+  int counted{};
+  bool multiplication{ true };
+};
+
 class Day11
 {
 public:
+  std::vector<Monkey> extract();
+  void solveA(std::vector<Monkey> monkeys);
+  void solveB(std::vector<Monkey> monkeys);
   void solve();
 };
